34ce95977978f4418c5f2ff0e3183558dc0fc86d
NepuShiro <nepushiro@nepunep.xyz>
2025-08-29 12:01:45 -0500

    Initial Commit

---
+++ BepisLocaleLoader/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.NET.Common;
+using BepInExResoniteShim;
+using FrooxEngine;
+using HarmonyLib;
+
+namespace BepisLocaleLoader;
+
+[ResonitePlugin(PluginMetadata.GUID, PluginMetadata.NAME, PluginMetadata.VERSION, PluginMetadata.AUTHORS, PluginMetadata.REPOSITORY_URL)]
+[BepInDependency(BepInExResoniteShim.PluginMetadata.GUID, BepInDependency.DependencyFlags.HardDependency)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+
+        // TODO: Switch this to a event subscription when BepInExResoniteShim is updated
+        Task.Run(async () =>
+        {
+            while (Engine.Current == null || Userspace.UserspaceWorld == null)
+            {
+                await Task.Delay(10);
+            }
+
+            await Task.Delay(5000);
+
+            if (NetChainloader.Instance.Plugins.Count <= 0) return;
+
+            NetChainloader.Instance.Plugins.Values.Do(LocaleLoader.AddLocaleFromPlugin);
+        });
+
+        Log.LogInfo($"Plugin {PluginMetadata.GUID} is loaded!");
+    }
+}
